d078a2382984bca423dd95522fb0cfaebb3db4dd
Kishore2212061 <mersalkishore79@gmail.com>
2025-01-28 15:41:43 +0530

    fir

--- src/Styles.tsx
+++ src/Styles.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container=styled.div`
 display:flex;
-background-color: black;
+background-color: wheat;
 padding:2rem;
 border-radius:20px;
 max-width:1920px;
